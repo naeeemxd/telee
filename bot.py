f52effab2245e9bde5ce59b5186f67e41c485103
Naeem <58908219+naeeemxd@users.noreply.github.com>
2025-09-27 21:39:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
@@ -13,8 +14,22 @@ import firebase_admin
 
 Token = '5873483525:AAFBY74pem2W2iGONEhpEz6yN09vCmqqy0Q'
 # db = firestore.client()
-cred = credentials.Certificate("serviceAccountKey.json")
+cred_dict = {
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID", ""),
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID", ""),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI", ""),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI", ""),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL", ""),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL", "")
+}
+
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
+
 db = firestore.client()
 app_web = Flask(__name__)
 
